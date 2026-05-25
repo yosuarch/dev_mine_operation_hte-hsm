559dfc835e818b75f44d1f7a5e57f0c94bd28e2b
yosuarch <yosuarch@hotmail.com>
2026-05-25 19:54:10 +0900

    dev-1.0 : starting construct the database

---
+++ app/Database/Migrations/2026-05-25-101310_GeneralInfo.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use PHPUnit\Framework\Constraint\Constraint;
+
+class GeneralInfo extends Migration
+{
+
+    // common information
+    protected $table = 'site_code';
+
+    public function up()
+    {
+        // create the table
+        $this->forge->addField([
+            'idx' => [
+                'type' => 'int',
+                'constraint' => 3,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'code' => [
+                'type' => 'varchar',
+                'constraint' => 32,
+                'null' => false,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('idx');
+        $this->forge->addUniqueKey('code', "uq_" . $this->table);
+        $this->forge->createTable($this->table);
+    }
+
+    public function down()
+    {
+        // drop the table
+        $this->forge->dropTable($this->table);
+    }
+}
---
+++ app/Database/Migrations/2026-05-25-101330_EquipmentRegister.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class EquipmentRegister extends Migration
+{
+    // common info
+    protected $table = 'dept_code';
+
+    public function up()
+    {
+        // generate table
+        // $this->forge->addField([
+        //     'idx' => [
+        //         'idx' => [
+        //             'type' => 'int',
+        //             'constraint' => 3,
+        //             'unsigned' => true,
+        //             'auto_increment' => true,
+        //         ],
+        //         'code'
+        //     ],
+        // ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
