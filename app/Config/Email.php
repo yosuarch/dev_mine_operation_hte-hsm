9583c08544da35e7ed360b37ba4c55f34d323a0e
yosuarch <yosuarch@hotmail.com>
2026-05-25 19:05:39 +0900

    configured: (1)database connection (2)session save path (3)codeigniter-shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'yosuarch@gmail.com';
+    public string $fromName   = 'mine-operation';
     public string $recipients = '';
 
     /**
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,5 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+service('auth')->routes($routes);
