be18b77d1549f08775e70ef4553f43e2b18a8625
yosuarch <yosuarch@hotmail.com>
2026-05-30 08:57:42 +0900

    20260530_v4: fixing the upload at PSI_RECORD. load all the neccesarry data into memory and search from it and improving the INSERT logic

--- app/Views/pages/psi/modal-psi.php
+++ app/Views/pages/psi/modal-psi.php
@@ -1,17 +1,97 @@
 <!-- Modal -->
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-fullscreen">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Pre-Start Inspection Import</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="psiForm" method="post" action="/upload-psi-record" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3">
+                                <label for="formFile" class="form-label">Select the Pre-Start Inspection (P2H) File</label>
+                                <input class="form-control" type="file" id="formFile" name="psiRecording">
+                            </div>
+                            <p><small>only excel file less than 5mb can be upload</small></p>
+                            <button type="button" id="previewPSIFile" class="btn btn-outline-info">Preview</button>
+                        </div>
+                        <div class="col-lg-8">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div class="col-lg-12">
+                            <!-- preview table -->
+                            <table id="filePreview" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Equipment ID</th>
+                                        <th>Date</th>
+                                        <th>Shift</th>
+                                        <th>Operator Name</th>
+                                        <th>HM Start</th>
+                                        <th>HM End</th>
+                                        <th>Checking Item</th>
+                                        <th>Checking Note</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- body from json -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-warning">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- reporting the PSI -->
+<div class="modal fade" id="generateReportModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="generateReportModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="generateReportModalLabel">Pre-Start Inspection Import</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="row">
+                    <div class="col-lg-2 border-end">
+                        <div class="mb-3">
+                            <label class="form-label">Select Date Range</label>
+                            <input type="text" id="datePicker" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-lg-10 border-end">
+                        <p>choose equipment</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At corporis reprehenderit consequuntur voluptatem atque officiis ex nostrum veritatis! Sunt, perspiciatis vel! Temporibus omnis suscipit ratione doloribus reprehenderit quidem dicta ut nisi veniam fugiat dolore eligendi, corrupti dolorum harum beatae autem inventore recusandae molestiae praesentium esse nostrum vero distinctio, dignissimos illo. Impedit saepe itaque autem ratione quibusdam praesentium rem! Sequi obcaecati eos omnis corrupti consequuntur quis laudantium. Atque tenetur culpa doloribus consequuntur sequi recusandae. Ea, blanditiis.</p>
+                    </div>
+                    <br>
+                    <div class="col-lg-4 border-start">
+                        <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo omnis rem possimus vitae minus blanditiis distinctio, quibusdam cupiditate laboriosam enim unde laborum ex temporibus dolores recusandae excepturi, a id voluptate?</small></p>
+                    </div>
+                    <div class="col-lg-8">
+                        <p><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam tempora facilis laboriosam quod animi doloremque vel modi reprehenderit placeat, eos perspiciatis est impedit maiores. Provident fuga itaque illo deleniti modi?</small></p>
+                    </div>
+                    <div class="col-lg-8">
+                        <p><small>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi magni sequi eveniet autem at delectus iste alias fugit neque aliquam minima quas illum laudantium odit, molestiae illo dolore eligendi nesciunt!</small></p>
+                    </div>
+                    <div class="col-lg-4">
+                        <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo omnis rem possimus vitae minus blanditiis distinctio, quibusdam cupiditate laboriosam enim unde laborum ex temporibus dolores recusandae excepturi, a id voluptate?</small></p>
+                    </div>
+                    <div class="col-lg-12">
+                        <p><small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquam doloremque ad libero dolore facilis itaque aperiam hic animi modi labore autem esse et, voluptate eveniet molestias voluptatem nam? Nulla modi incidunt labore, ad vitae facilis voluptas tempore. Quasi enim ducimus vel maxime quam explicabo voluptatum rem, deleniti porro id accusamus dolorem qui consequatur sit unde. Natus, quod doloribus? Quo, tempore deleniti iusto aliquam consequuntur illo pariatur autem accusamus veniam atque voluptates labore amet reprehenderit laudantium. Necessitatibus doloribus aliquam, voluptatum delectus sunt harum praesentium sint tempora quod cupiditate provident veniam. Similique quas eius sint temporibus qui adipisci, pariatur mollitia minima sequi eos repellat error, ab labore ullam laborum ducimus aut atque doloribus porro id iste maxime. Ipsa ullam id incidunt soluta veniam. Praesentium voluptatem qui, dolore cumque saepe nemo hic placeat quibusdam amet odio, maiores eligendi soluta sapiente similique! Odio, quasi! Debitis, laudantium quaerat. Ab eius pariatur dicta animi unde cum illum tempora modi a consequatur numquam accusamus neque sint nulla, dolor obcaecati dolore quaerat. Hic perspiciatis quo necessitatibus consectetur officia deleniti porro nisi expedita, recusandae velit optio, animi eum nam excepturi autem quaerat! Pariatur eaque laboriosam ullam, itaque eum unde numquam dicta consequatur consequuntur deserunt quisquam, fugit repellendus tempora rerum nulla? Sed hic earum, debitis, dolorem accusantium eum cumque eius perferendis sint incidunt assumenda aliquam dicta quos recusandae culpa tenetur beatae praesentium necessitatibus! Eaque nostrum delectus unde qui aut corrupti explicabo expedita in cupiditate, similique alias reprehenderit voluptatibus sit commodi minima molestias! Perspiciatis nihil accusantium quos, temporibus facere deserunt. Facere expedita id autem excepturi, enim tempora, odio voluptatum aliquid ea doloribus consequuntur tenetur neque quae nemo temporibus dolor consequatur corporis quo beatae quisquam. Sequi, sapiente. Debitis quae fugit, possimus sint facilis veritatis exercitationem cumque harum pariatur quasi sapiente suscipit ad aspernatur, doloremque obcaecati vel. Exercitationem voluptatibus perferendis cum veritatis.</small></p>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
+                <button type="submit" class="btn btn-warning">Submit</button>
             </div>
         </div>
     </div>
