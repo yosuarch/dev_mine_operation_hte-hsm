99143861c228ff99aa35f3428fc9935cdcde56a7
yosuarch <yosuarch@hotmail.com>
2026-05-30 06:55:41 +0900

    20260530_v1 : fixing the upload at PSI_RECORD. load all the neccesarry data into memory and search from it

--- app/Controllers/PrestartInspection/UploadPSIRecord.php
+++ app/Controllers/PrestartInspection/UploadPSIRecord.php
@@ -26,14 +26,16 @@ class UploadPSIRecord extends BaseController
             $spreasheet = IOFactory::load($filePath);
             $sheetData = $spreasheet->getActiveSheet()->toArray(null, true, true, true);
 
+            // connect to database
+            $db = \Config\Database::connect();
+
+            $equipList = array_column($db->table('equipment_register')->select("CONCAT(text_code, num_code) as code, idx")->get()->getResultArray(), 'idx', 'code');
+            $shiftList = array_column($db->table('general_working_shift')->select("code, idx")->get()->getResultArray(), 'idx', 'code');
+            $mpList    = array_column($db->table('mp_list')->select("name, idx")->get()->getResultArray(), 'idx', 'name');
+            $partList  = array_column($db->table('psi_unique_observed_item')->select("checking_part_idn, idx")->get()->getResultArray(), 'idx', 'checking_part_idn');
+
             // prepare to insert, extracting data for each row
             $model = new ModelPsiRecord();
-            // load the table from database
-            $db = \Config\Database::connect();
-            $equipmentRegister = $db->table('equipment_register');
-            $workingShift = $db->table('general_working_shift');
-            $manPowerList = $db->table('mp_list');
-            $checkPart = $db->table('psi_unique_observed_item');
 
             $inserted = 0;
             $skipped = 0;
@@ -48,65 +50,57 @@ class UploadPSIRecord extends BaseController
                 $dateFormated = Date::excelToDateTimeObject($excelDate)->format('Y-m-d');
 
                 // transforming the equipment_id
-                // 1. get the value from column B
-                $rawECode = $row['B'];
-                $rawShift = $row['D'];
-                $rawOpName = $row['E'];
-                $rawCheckPart = $row['H'];
-                $rawCheckStatus = $row['I'];
+                // 1. search the result at memory
+                $equipIdx = $equipList[$row['B']] ?? null;
+                $shiftIdx = $shiftList[$row['D']] ?? null;
+                $opIdx    = $mpList[$row['E']] ?? null;
+                $partIdx  = $partList[$row['H']] ?? null;
 
                 // 2. get iterate for each row
                 // 2.1 equipment_ID
-                $rawECode = $equipmentRegister->select('idx')
-                    ->where("CONCAT(text_code, num_code) =", $rawECode)
-                    ->get()
-                    ->getRow();
+                // $rawECode = $equipmentRegister->select('idx')
+                //     ->where("CONCAT(text_code, num_code) =", $rawECode)
+                //     ->get()
+                //     ->getRow();
 
-                // 2.2 shift
-                $rawShift = $workingShift->select('idx')
-                    ->where("code =", $rawShift)
-                    ->get()
-                    ->getRow();
+                // // 2.2 shift
+                // $rawShift = $workingShift->select('idx')
+                //     ->where("code =", $rawShift)
+                //     ->get()
+                //     ->getRow();
 
-                // 2.3 operator name
-                $rawOpName = $manPowerList->select('idx')
-                    ->where("name =", $rawOpName)
-                    ->get()
-                    ->getRow();
+                // // 2.3 operator name
+                // $rawOpName = $manPowerList->select('idx')
+                //     ->where("name =", $rawOpName)
+                //     ->get()
+                //     ->getRow();
 
-                // 2.3 checking part
-                $rawCheckPart = $checkPart->select('idx')
-                    ->where("checking_part_idn =", $rawCheckPart)
-                    ->get()
-                    ->getRow();
+                // // 2.3 checking part
+                // $rawCheckPart = $checkPart->select('idx')
+                //     ->where("checking_part_idn =", $rawCheckPart)
+                //     ->get()
+                //     ->getRow();
 
-                // 2.4 boolean check status
-                $checkStatus = (strcasecmp($rawCheckStatus, 'TRUE') === 0) ? 1 : 0;
-
-                // 3. if found then use the idx, else return the not match value and continue the existing data
-                if ($rawECode && $rawShift && $rawOpName && $rawCheckPart) {
+                if ($equipIdx && $shiftIdx && $opIdx && $partIdx) {
                     $data = [
-                        'equipment_id' => (int)$rawECode->idx,
-                        'date' => $dateFormated,
-                        'shift' => (int)$rawShift->idx,
-                        'operator_name' => (int)$rawOpName->idx,
+                        'equipment_id'    => (int)$equipIdx,
+                        'date'            => Date::excelToDateTimeObject($row['C'])->format('Y-m-d'),
+                        'shift'           => (int)$shiftIdx,
+                        'operator_name'   => (int)$opIdx,
                         'hourmeter_start' => is_numeric($row['F']) ? (int)$row['F'] : 0,
                         'hourmeter_end'   => is_numeric($row['G']) ? (int)$row['G'] : 0,
-                        'checking_part' => (int)$rawCheckPart->idx,
-                        'checking_status' => (int)$checkStatus,
-                        'checking_note' => (string)$row['J'],
+                        'checking_part'   => (int)$partIdx,
+                        'checking_status' => (strcasecmp($row['I'], 'TRUE') === 0) ? 1 : 0,
+                        'checking_note'   => (string)$row['J'],
                     ];
                     if ($model->insert($data)) {
                         $inserted++;
                     } else {
-                        $errors[] = "Row $index: Insert failed - " . implode(', ', $model->errors());
+                        $errors[] = "Row $index: Insert failed";
                     }
                 } else {
                     $skipped++;
-                    $errors[] = "Row $index: Missing lookup - Equipment: " . ($rawECode ? 'OK' : 'FAIL') .
-                        ", Shift: " . ($rawShift ? 'OK' : 'FAIL') .
-                        ", Operator: " . ($rawOpName ? 'OK' : 'FAIL') .
-                        ", CheckPart: " . ($rawCheckPart ? 'OK' : 'FAIL');
+                    $errors[] = "Row $index: Lookup failed (Check data integrity)";
                 }
             }
 
