540459af8e38de1376194b87b8469d82f84a1894
yosuarch <yosuarch@hotmail.com>
2026-05-30 13:24:22 +0900

    202260530: beta-release, still need many improvement but it works to recieve the record and store at database. disable a few button

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -4,7 +4,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <!-- <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
@@ -16,6 +16,6 @@
                     <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
-        </div>
+        </div> -->
     </div>
 </nav>
--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -3,9 +3,9 @@
         <a class="nav-link" href="/">Dashboard</a>
         <a class="nav-link" href="/prestart-insepction">Pre-Start Inspection</a>
         <a class="nav-link" href="/manpower">Manpower</a>
-        <a class="nav-link" href="#">Menu Item 3</a>
+        <!-- <a class="nav-link" href="#">Menu Item 3</a> -->
         <hr class="my-2">
-        <a class="nav-link" href="#">Settings</a>
+        <!-- <a class="nav-link" href="#">Settings</a> -->
         <a class="nav-link" href="<?= url_to('logout'); ?>">Logout</a>
     </nav>
 </aside>
